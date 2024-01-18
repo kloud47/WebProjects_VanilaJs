e7904f9015511ff667e4638e513fa345d168491e
kloud47 <136368351+kloud47@users.noreply.github.com>
2024-01-18 12:35:32 +0530

    Adding the oops js

---
+++ JS_OOPS/challenge.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// const Car = function(make, speed) {
+//     this.speed = `${speed} km/h`;
+//     this.make = make;
+// };
+
+// Car.prototype.accelerate = function() {
+//     this.speed = `${parseInt(this.speed) + 10} km/h`;
+//     console.log('New speed', this.speed);
+// };
+
+// Car.prototype.brake = function() {
+//     this.speed = `${parseInt(this.speed) - 5} km/h`;
+//     console.log('New speed', this.speed);
+// };
+
+// const BMW = new Car('BMW', 120);
+// BMW.accelerate();
+// BMW.accelerate();
+// BMW.brake();
+
+console.log(null ?? 1);
